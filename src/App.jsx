c05ab1b98c5c8ca8acf055d83f3121bbc8682dd6
Sahil Sharma <sahil.s.sharma22@gmail.com>
2025-01-06 17:54:31 +0530

    Onboarding routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Login from "./Auth/pages/Login";
 import Pagenotfound from "./404Page/Pagenotfound";
 import Recruitment from "./SuperAdmin/Recruitment/pages/Recruitment";
 import Employee from "./SuperAdmin/Employee/Pages/Employee";
+import Onboarding from "./SuperAdmin/Onboarding/Pages/Onboarding";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +22,7 @@ function App() {
         <Route path="/home/*" element={<SuperAdminDashboard />} />
         <Route path="/recruitment/*" element={<Recruitment />} />
         <Route path="/employee/*" element={<Employee />}/>
+        <Route path="/onboarding/*" element={<Onboarding />}/>
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </>

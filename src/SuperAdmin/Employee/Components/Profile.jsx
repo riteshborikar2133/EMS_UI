1f7acd4438cf9aae7e3701d835920cd94cc76e16
Sahil Sharma <sahil.s.sharma22@gmail.com>
2025-01-07 17:00:53 +0530

    Employee profile done

--- src/SuperAdmin/Employee/Components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Profile() {
-  return (
-    <div className='box-border border bg-white p-3 px-6 rounded-lg shadow-sm w-full my-3'>
-      Employee Profile
-    </div>
-  )
-}
